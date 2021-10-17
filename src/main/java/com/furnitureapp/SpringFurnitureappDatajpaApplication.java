fa2a97b9eb5c21ebf3bfe8f115fcefd662d1f2c5
GayathriS <sai15gayathri@gmail.com>
2021-10-17 12:12:23 +0530

    New Commit

--- src/main/java/com/furnitureapp/SpringFurnitureappDatajpaApplication.java
+++ src/main/java/com/furnitureapp/SpringFurnitureappDatajpaApplication.java
@@ -1,14 +1,8 @@
 package com.furnitureapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.furnitureapp.service.IBrandService;
-import com.furnitureapp.service.ICategoryService;
-import com.furnitureapp.service.IDetailsService;
-import com.furnitureapp.service.IFurnitureService;
 
 /**
  * @author GayathriS
@@ -18,18 +12,6 @@ import com.furnitureapp.service.IFurnitureService;
 @SpringBootApplication
 public class SpringFurnitureappDatajpaApplication implements CommandLineRunner {
 
-	@Autowired
-	IFurnitureService furnitureService;
-
-	@Autowired
-	IDetailsService detailsService;
-
-	@Autowired
-	IBrandService brandService;
-
-	@Autowired
-	ICategoryService categoryService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFurnitureappDatajpaApplication.class, args);
 	}
@@ -37,7 +19,6 @@ public class SpringFurnitureappDatajpaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		
 	}
 
 }
